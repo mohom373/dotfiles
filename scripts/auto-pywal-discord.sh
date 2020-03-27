120078f517a9c820cdf63bfbfb5b3babc9f5f068
mohom373 <mohom373@student.liu.se>
2020-03-27 23:38:42 +0100

    Polybar icons work now

--- scripts/auto-pywal-discord.sh
+++ scripts/auto-pywal-discord.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 wal -q -i $HOME/Wallpapers/ -o pywal-discord
 cp $HOME/.config/BetterDiscord/themes/pywal-discord.theme.css $HOME/snap/discord/109/.config/BetterDiscord/themes/pywal-discord.theme.css
-spicetify -q update 
+/home/linuxbrew/.linuxbrew/bin/spicetify -q apply
