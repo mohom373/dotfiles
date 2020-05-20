1bf5b59c0d060402a02bb8496a55b95f6fdc96bb
Mohammad <mohom373@student.liu.se>
2020-05-20 23:57:48 +0200

    added a few changed

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload -q white &
+    MONITOR=$m polybar --reload -q desktop &
 done
 # Launch bar 
 #polybar white -r &
